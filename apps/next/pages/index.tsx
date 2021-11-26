6a894b483e00cb2f29cce8afc5d89571bdc8ff0e
cisaias <cisaias@uninorte.edu.co>
2021-11-25 23:32:48 -0500

    last changes
    last changes

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -2,18 +2,36 @@ import styles from './index.module.scss';
 import Link from 'next/link';
 import {Ui} from '@final-uninorte/ui'
 import Countries from '../components/countries/countries';
-export function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.scss file.
-   */
+import { request, gql } from 'graphql-request'
+import { IndexProps } from '@final-uninorte/types';
+
+const query = gql`
+    query {
+    countries {
+      code
+      name
+    }
+  }
+`
+export async function getStaticProps() {
+  const data = await request('https://countries.trevorblades.com/', query);
+  const {countries} = data
+
+  return {
+    props: {
+      countries
+    },
+  }
+}
+
+export function Index(props: IndexProps) {
+  const {countries} = props;
   return (
     <div className={styles.main}>
       <Ui title="Bienvenido rey" showtitle={true}/>
       <Link href="/about"> Nosotros
       </Link>
-      <Countries/>
+      <Countries  countries={countries}/>
     </div>
   );
 }
